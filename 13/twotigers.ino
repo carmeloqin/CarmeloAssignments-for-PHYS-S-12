a9cc666f141f58bffe249b56335610af0901fe73
carmeloqin <carmeloqin@icloud.com>
2019-08-02 14:29:54 -0400

    1429

---
+++ 13/twotigers.ino
@@ -0,0 +1,74 @@
+int buzzerPin = 2;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  tone(buzzerPin, 261.63, 500);
+  delay(500);
+  tone(buzzerPin, 293.66, 500);
+  delay(500);
+  tone(buzzerPin, 329.63, 500);
+  delay(500);
+  tone(buzzerPin, 261.63, 500);
+  delay(550);
+  tone(buzzerPin, 261.63, 500);
+  delay(500);
+  tone(buzzerPin, 293.66, 500);
+  delay(500);
+  tone(buzzerPin, 329.63, 500);
+  delay(500);
+  tone(buzzerPin, 261.63, 500);
+  delay(550);
+  tone(buzzerPin, 329.63, 500);
+  delay(500);
+  tone(buzzerPin, 349.23, 500);
+  delay(500);
+  tone(buzzerPin, 392, 1000);
+  delay(1000);
+  tone(buzzerPin, 329.63, 500);
+  delay(500);
+  tone(buzzerPin, 349.23, 500);
+  delay(500);
+  tone(buzzerPin, 392, 1000);
+  delay(1050);
+  tone(buzzerPin, 392, 375);
+  delay(375);
+  tone(buzzerPin, 440, 125);
+  delay(125);
+  tone(buzzerPin, 392, 375);
+  delay(375);
+  tone(buzzerPin, 349.23, 125);
+  delay(125);
+  tone(buzzerPin, 329.63, 500);
+  delay(500);
+  tone(buzzerPin, 261.63, 500);
+  delay(550);
+  tone(buzzerPin, 392, 375);
+  delay(375);
+  tone(buzzerPin, 440, 125);
+  delay(125);
+  tone(buzzerPin, 392, 375);
+  delay(375);
+  tone(buzzerPin, 349.23, 125);
+  delay(125);
+  tone(buzzerPin, 329.63, 500);
+  delay(500);
+  tone(buzzerPin, 261.63, 500);
+  delay(550);
+  tone(buzzerPin, 261.63, 500);
+  delay(500);
+  tone(buzzerPin, 196, 500);
+  delay(500);
+  tone(buzzerPin, 261.63, 1000);
+  delay(1050);
+  tone(buzzerPin, 261.63, 500);
+  delay(500);
+  tone(buzzerPin, 196, 500);
+  delay(500);
+  tone(buzzerPin, 261.63, 1000);
+  delay(1050);
+}
